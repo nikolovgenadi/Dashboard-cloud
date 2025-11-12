c75500379f4e919d3a51e590b7d7f60b7f8e1869
Genadi Nikolov <genadi123.nik@gmail.com>
2025-11-12 06:23:20 +0100

    SCRUM-5 Add created_at date customer component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -217,6 +217,7 @@ export async function fetchFilteredCustomers(query: string) {
 		  customers.name,
 		  customers.email,
 		  customers.image_url,
+		  customers.created_at,
 		  COUNT(invoices.id) AS total_invoices,
 		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
 		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
@@ -225,7 +226,7 @@ export async function fetchFilteredCustomers(query: string) {
 		WHERE
 		  customers.name ILIKE ${`%${query}%`} OR
         customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url, customers.created_at
 		ORDER BY customers.name ASC
 	  `;
 
